31acb0da2f110369e4ed401ee413c400df2b10ff
Andrew <s0lbjorandr3w@gmail.com>
2026-09-20 22:55:16 -0700

    Step 2

--- samosa-selector/samosa-selector/src/App.jsx
+++ samosa-selector/samosa-selector/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
 
   return (
     <div className="App">
       <div className="header">
         <h1>Samosa Selector</h1>
-        <h2>Count: </h2>
-        <img className="samosa" src="https://imgs.search.brave.com/rwNlNTr6Im29jjHpCw4KlE2xHmSV8-JnnvtdRek3zuQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQy/MDA2MDM4Ny9waG90/by92ZWdldGFyaWFu/LWFsb28tc2Ftb3Nh/LW9yLXNhbW9zYXMt/aW5kaWFuLXNwZWNp/YWwtdHJhZGl0aW9u/YWwtc3RyZWV0LWZv/b2QtZmFtb3VzLWlu/ZGlhbi5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9clc4eXdD/UXBwWC1pLTBKZ1BF/SFhGdnBrMHdITGlw/T0JpWVpqVjlnV2hy/ND0" alt="Samosa" />
+        <h2>Count: {count}</h2>
+        <img className="samosa" onClick = {updateCount} src="https://imgs.search.brave.com/rwNlNTr6Im29jjHpCw4KlE2xHmSV8-JnnvtdRek3zuQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQy/MDA2MDM4Ny9waG90/by92ZWdldGFyaWFu/LWFsb28tc2Ftb3Nh/LW9yLXNhbW9zYXMt/aW5kaWFuLXNwZWNp/YWwtdHJhZGl0aW9u/YWwtc3RyZWV0LWZv/b2QtZmFtb3VzLWlu/ZGlhbi5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9clc4eXdD/UXBwWC1pLTBKZ1BF/SFhGdnBrMHdITGlw/T0JpWVpqVjlnV2hy/ND0" alt="Samosa" />
 
       </div>
     </div>
